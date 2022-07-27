2b4122a667fe82bd0ecd01f7d4d3b456db7f8d87
Maximiliano <87541149+m2x1m1li2no@users.noreply.github.com>
2022-07-26 23:10:32 -0300

    Exercise_1

---
+++ Tarea/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String theString = "the String";
+        int theNumber = 5;
+        double theDouble = 5.55;
+        long theLong = 1123456789;
+        boolean theBoolean = true;
+
+        System.out.println(theBoolean);
+        System.out.println(theDouble);
+        System.out.println(theNumber);
+        System.out.println(theLong);
+    }
+}
